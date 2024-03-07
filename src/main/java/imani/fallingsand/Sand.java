48fde80b06d29e01ceba608798933a5bd363f6a0
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-03-06 19:15:56 -0500

    changes

--- src/main/java/imani/fallingsand/Sand.java
+++ src/main/java/imani/fallingsand/Sand.java
@@ -1,13 +1,17 @@
 package imani.fallingsand;
 
 public class Sand {
-    private int[][] field = new int[3][3];
+    private int[][] field;
+
+    public Sand(int width, int height) {
+        field = new int[height][width];
+    }
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
 
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < field.length; y++) {
+            for (int x = 0; x < field[y].length; x++) {
                 builder.append(field[y][x]); // the one that prints sideways is x
             }
             builder.append("\n");
@@ -27,25 +31,24 @@ public class Sand {
 
         int[][] newField = new int[3][3];
 
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                if (y != 2) {
-                    if (field[y][x] == 1) {
-                        newField[y][x] = 1;
-                        if (field[y + 1][x] == 0) {
-                            newField[y][x] = 0;
-                            newField[y + 1][x] = 1;
-                        }
-                        else {
-                            newField[y + 1][x] = 1;
-                        }
-                    }
+        for (int y = field.length - 2; y >= 0; y--) {
+            for (int x = 0; x < field[y].length; x++) {
+                if (field[y][x] == 1 && field[y + 1][x] == 0) {
+                    field[y + 1][x] = 1;
+                    field[y][x] = 0;
                 }
             }
         }
-
-        field = newField;
-
     }
 }
+
+
+// settings actions on save - reformat optimize rearrange
+// do it backwards
+// intellij plugin checkstyles
+// dont want to touch the bottom row
+// gradle: id 'checkstlyle' reload
+// make it configurable
+
+
 
